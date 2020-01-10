22d3863d099d0480f52b534d379a0d0dbb6d1637
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-01-10 17:57:13 +0300

    Add property binding to the quote from parent component

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Quote } from "../quote";
 
 @Component({
-  selector: 'app-quote-detail',
-  templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  selector: "app-quote-detail",
+  templateUrl: "./quote-detail.component.html",
+  styleUrls: ["./quote-detail.component.css"]
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input() quote: Quote;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
